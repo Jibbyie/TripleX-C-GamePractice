bb7fc70db32be48cf52e8a92e1c2cb282b8c2f61
Jibril Abdulazeez <jibbyie@gmail.com>
2021-03-12 15:26:16 +0000

    Comments

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,15 +2,21 @@
 
 int main()
 {
+    // Expression Statements - Game introduction for player
     std::cout << "The codes, punch them in Agent!" << std::endl;
     std::cout << "You need to enter the correct codes to continue.." << std::endl;
 
-    int a = 4;
-    int b = 3;
-    int c = 5;
+    // Declaration Statements - Codes needed for player to enter
+    const int a = 4;
+    const int b = 3;
+    const int c = 5;
 
-    int sum = a + b + c;
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    // Print sum and product to the terminal
     std::cout << sum << std::endl;
+    std::cout << product << std::endl;
 
     return 0;
 }
