f75534518e52629e5f73833e98efd0b664bf70d9
Jibril Abdulazeez <jibbyie@gmail.com>
2021-03-12 14:46:18 +0000

    Basic C++ Practice

---
+++ TripleX.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "The codes, punch them in Agent!" << std::endl;
+    std::cout << "You need to enter the correct codes to continue.." << std::endl;
+
+    int a = 4;
+    int b = 3;
+    int c = 5;
+
+    int sum = a + b + c;
+    std::cout << sum << std::endl;
+
+    return 0;
+}
