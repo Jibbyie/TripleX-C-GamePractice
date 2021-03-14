56e7d25d9c69762d051350f6b4c77230f247bed9
Jibril Abdulazeez <jibbyie@gmail.com>
2021-03-14 23:34:47 +0000

    Player Input - cin extraction operator

--- TripleX.cpp
+++ TripleX.cpp
@@ -18,9 +18,15 @@ int main()
     std::cout << "+ There are 3 numbers in the code" << std::endl;
     std::cout << "+ The codes add up to: " << CodeSum << std::endl;
     std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
-    std::cout << "Think long and carefully.. " << std::endl;
+    std::cout << "Think long and carefully.. " << std::endl; std::cout << "Enter in your guesses seperated by a space or on seperate lines:" << std::endl;
 
-    int PlayerGuess;
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
 
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+    
     return 0;
 }
