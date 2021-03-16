50133b3ad09a732ed5e591f145ef681c84159442
Jibril Abdulazeez <jibbyie@gmail.com>
2021-03-16 12:07:45 +0000

    \n, functions, if and else statement

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-
-int main()
+void PrintIntroduction()
 {
-    // Expression Statements - Game introduction for player
+     // Expression Statements - Game introduction for player
     std::cout << "The codes, punch them in Agent! We are all riding on you now." << std::endl;
     std::cout << "You must enter the correct codes to continue, or all will be lost.. " << std::endl;
+}
+
+void PlayGame()
+{
+    PrintIntroduction();
 
     // Declaration Statements - Codes needed for player to enter
     const int CodeA = 4;
@@ -15,18 +19,31 @@ int main()
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Print sum and product to the terminal
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
-    std::cout << "Think long and carefully.. " << std::endl; std::cout << "Enter in your guesses seperated by a space or on seperate lines:" << std::endl;
+    std::cout << "+ There are 3 numbers in the code";
+    std::cout << "\n+ The codes add up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct;
+    std::cout << "\nThink long and carefully.. \n"; std::cout << "Enter in your guesses seperated by a space or on seperate lines:\n";
 
+    // Store Player Guess
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
     
+    // Check Player Guess 
+    if(GuessSum == CodeSum & GuessProduct == CodeProduct) 
+    {
+        std::cout << "Your Guesses are correct!\n";
+    } 
+    else 
+    {
+        std::cout << "All is lost..\n";
+    }
+}
+
+int main()
+{
+    PlayGame();
     return 0;
 }
